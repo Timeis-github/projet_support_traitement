7a0a42772bcaa36f9626bdd3d5560db897cb46a2
Timeis-github <54853240+Timeis-github@users.noreply.github.com>
2025-12-15 11:48:11 +0100

    Set up Spark context and read CSV file
    Initialize Spark context and measure execution time for reading a CSV file.

--- longest_jobs.py
+++ longest_jobs.py
@@ -1,1 +1,15 @@
 # TODO : Create a RDD with the top 10% longest jobs in execution time
+
+from pyspark import SparkContext, SparkConf
+from timeit import default_timer as timer
+
+conf = SparkConf().setAppName("projet")
+sc = SparkContext(conf=conf)
+sc.setLogLevel("ERROR")
+
+start = timer()
+
+lines=sc.textFile("/user/fzanonboito/CISD/darshan/9/Darshan.csv")
+
+end = timer()
+print(end - start)
